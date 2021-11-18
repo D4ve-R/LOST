340ef4f22eb0d882515374d12b34ab3b1e898415
JanoschLentz <Janosch-Lentz@gmx.de>
2021-11-18 13:22:10 +0100

    App Class ActionListener implementiert um Intro Panel nach 4 sek zu ändern. Prototyp für MainMenu Class panel Package erstellt ResourceHandler Interface im Package utils erstellt

--- macpan/app/src/main/java/lost/macpan/App.java
+++ macpan/app/src/main/java/lost/macpan/App.java
@@ -5,16 +5,26 @@
 
 package lost.macpan;
 import lost.macpan.panel.Intro;
+import lost.macpan.panel.MainMenu;
 
+import javax.swing.Timer;
 import javax.swing.JFrame;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class App extends JFrame{
+public class App extends JFrame implements ActionListener{
 
     private static final int width = 950;
     private static final int height = 700;
+    private static final int serializeId = 123456789;
+    private Timer timer;
 
     public App(){
+        int delay = 5500; //müssen testen um auf 4 Sekunden zu kommen jz ca. 2 Sek
+
+        timer = new Timer(delay, this);
+
         setTitle("MacPan");
         setMinimumSize(new Dimension(width, height));
         setLocationRelativeTo(null);
@@ -23,5 +33,15 @@ public class App extends JFrame{
         setResizable(false);
         pack();
         setVisible(true);
+
+        timer.start();
     }
+
+    @Override
+    public void actionPerformed ( ActionEvent evt) {
+        setContentPane(new MainMenu(this));
+        revalidate();
+        timer.stop();
+    }
+
 }
---
+++ macpan/app/src/main/java/lost/macpan/panel/MainMenu.java
@@ -0,0 +1,11 @@
+package lost.macpan.panel;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.awt.Color;
+
+public class MainMenu extends JPanel{
+    public MainMenu(JFrame frame){
+
+        setBackground(Color.green);
+    }
+}
