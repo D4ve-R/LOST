e79f603ad1ccac32ea59ba3b74c9bac25f91b64e
FatAko <fatihkeremakoglan@hotmail.com>
2021-11-20 18:21:06 +0100

    OptionsMenu soweit fertig, actionPerformed muss noch erweitert werden, wenn Tastenbelegungsfenster und Spielbeschreibungsfenster erstellt werden ( + Bilder wurden hinzugefügt)

--- macpan/app/src/main/java/lost/macpan/panel/MainMenu.java
+++ macpan/app/src/main/java/lost/macpan/panel/MainMenu.java
@@ -7,17 +7,19 @@
  */
 
 package lost.macpan.panel;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
 import lost.macpan.utils.ResourceHandler;
 
 /**
  * Die Klasse MainMenu stellt das Haputmenü des Spiels auf dem JPanel dar.
  */
-public class MainMenu extends JPanel implements ActionListener,ResourceHandler {
+public class MainMenu extends JPanel implements ActionListener, ResourceHandler {
     // Erstellen der einzelnen Buttons
     private final JButton playBtn = new JButton("Spiel Starten");
     private final JButton loadBtn = new JButton("Spiel Laden");
@@ -33,29 +35,30 @@ public class MainMenu extends JPanel implements ActionListener,ResourceHandler {
 
     /**
      * Der Konstruktor MainMenu platziert die Bilder und Buttons, welche zum Hauptmenue gehören auf dem Frame
+     *
      * @param frame
      */
-    public MainMenu(JFrame frame){
+    public MainMenu(JFrame frame) {
         parentFrame = frame;
 
         try {
             img = ImageIO.read(getFileResourcesAsStream("images/panelImages/HauptmenuePlatzhalter.png"));
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        if(img != null) {
+        if (img != null) {
             label = new JLabel(new ImageIcon(img));
         }
 
         setLayout(null);
         // Positionierung der Buttons und Labels
-        label.setBounds(175,50,600,200);
-        playBtn.setBounds(395,300,160,50);
-        loadBtn.setBounds(395,360,160,50);
-        highscoresBtn.setBounds(395,420,160,50);
-        optionsBtn.setBounds(395,480,160,50);
-        quitBtn.setBounds(395,540,160,50);
+        label.setBounds(175, 50, 600, 200);
+        playBtn.setBounds(395, 300, 160, 50);
+        loadBtn.setBounds(395, 360, 160, 50);
+        highscoresBtn.setBounds(395, 420, 160, 50);
+        optionsBtn.setBounds(395, 480, 160, 50);
+        quitBtn.setBounds(395, 540, 160, 50);
         // Hinzufügen der Buttons und Labels auf den Frame
         add(label);
         add(playBtn);
@@ -72,8 +75,8 @@ public class MainMenu extends JPanel implements ActionListener,ResourceHandler {
     }
 
     @Override
-    public void actionPerformed(ActionEvent e){
-        if(e.getSource() == playBtn){
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == playBtn) {
 
         } else if (e.getSource() == loadBtn) {
 
@@ -83,6 +86,9 @@ public class MainMenu extends JPanel implements ActionListener,ResourceHandler {
             parentFrame.revalidate();
 
         } else if (e.getSource() == optionsBtn) {
+            OptionsMenu optionsMenu = new OptionsMenu(parentFrame,this.parentFrame.getContentPane());
+            parentFrame.setContentPane(optionsMenu);
+            parentFrame.revalidate();
 
         } else if (e.getSource() == quitBtn) {
             QuitMenu quitMenu = new QuitMenu(parentFrame);
---
+++ macpan/app/src/main/java/lost/macpan/panel/OptionsMenu.java
@@ -0,0 +1,61 @@
+package lost.macpan.panel;
+
+import lost.macpan.utils.ResourceHandler;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.imageio.ImageIO;
+import java.awt.*;
+
+public class OptionsMenu extends JPanel implements ActionListener, ResourceHandler {
+    private final JButton keyBtn = new JButton("Tastenbelegung");
+    private final JButton descBtn = new JButton("Spielbeschreibung");
+    private final JButton backBtn = new JButton("Zuruck");
+    private JLabel label;
+    private Image img;
+    private JFrame parentFrame;
+    private Container before;
+
+    public OptionsMenu(JFrame frame, Container beforeMenu) {
+        parentFrame = frame;
+        before = beforeMenu;
+        try {
+            img = ImageIO.read(getFileResourcesAsStream("images/panelImages/OptionenPlatzhalter.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (img != null) {
+            label = new JLabel(new ImageIcon(img));
+        }
+        setLayout(null);
+        label.setBounds(175, 50, 600, 200);
+        keyBtn.setBounds(395, 300, 160, 50);
+        descBtn.setBounds(395, 360, 160, 50);
+        backBtn.setBounds(30, 550, 160, 50);
+        add(backBtn);
+        add(label);
+        add(keyBtn);
+        add(descBtn);
+        keyBtn.addActionListener(this);
+        descBtn.addActionListener(this);
+        backBtn.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == backBtn) {
+            if (before instanceof MainMenu) {
+                MainMenu mainMenu = new MainMenu(parentFrame);
+                parentFrame.setContentPane(mainMenu);
+                parentFrame.revalidate();
+            } else if (before instanceof PauseMenu) {
+                //Spiel weiter Spielen
+            }
+        } else if (e.getSource() == keyBtn) {
+
+        } else if (e.getSource() == descBtn) {
+
+        }
+    }
+}
