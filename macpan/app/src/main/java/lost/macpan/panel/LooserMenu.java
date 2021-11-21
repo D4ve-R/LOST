cb109c54b142346dff9df2d861260a54503a8206
FatAko <fatihkeremakoglan@hotmail.com>
2021-11-21 18:31:14 +0100

    Gweninner und Verlierer - Fenster soweit Fertig. Funktion den Namen einzugeben und Score auszugeben fehlt noch.

---
+++ macpan/app/src/main/java/lost/macpan/panel/LooserMenu.java
@@ -0,0 +1,36 @@
+package lost.macpan.panel;
+
+import lost.macpan.utils.ResourceHandler;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LooserMenu extends JPanel implements ActionListener, ResourceHandler {
+    private JFrame parentFrame;
+    private JLabel label;
+    private Image img;
+    public LooserMenu(JFrame frame) {
+        parentFrame = frame;
+        try {
+            img = ImageIO.read(getFileResourcesAsStream("images/panelImages/GestorbenPlatzhalter.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (img != null) {
+            label = new JLabel(new ImageIcon(img));
+        }
+
+        //!Funktionalität Namen einzutragen und Score auszugeben fehlt noch
+
+        setLayout(null);
+        label.setBounds(175,50,600,200);
+        add(label);
+        setBackground(Color.DARK_GRAY);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+}
