5a499f829c4bfaf8ceb6b0aed49cf1cc299e891d
Sebastian <sese.menne@web.de>
2021-11-24 17:51:07 +0100

    Simplified StreamConverter

--- macpan/app/src/main/java/lost/macpan/utils/StreamConverter.java
+++ macpan/app/src/main/java/lost/macpan/utils/StreamConverter.java
@@ -1,18 +1,10 @@
 package lost.macpan.utils;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
-import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 
 public interface StreamConverter {
-    /*
-        http://www.java2s.com/Code/Android/File/getResourceAsStreamLoadstheresourcefromclasspath.htm
-     */
 
     /**
      * Wandelt einen Stream zu einem String
@@ -22,21 +14,6 @@ public interface StreamConverter {
      * @throws IOException throws an io exception when an error occurs
      */
     default String convertStreamToString(InputStream is) throws IOException {
-        if (is != null) {
-            Writer writer = new StringWriter();
-            char[] buffer = new char[1024];
-            try {
-                Reader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
-                int n;
-                while ((n = reader.read(buffer)) != -1) {
-                    writer.write(buffer, 0, n);
-                }
-            } finally {
-                is.close();
-            }
-            return writer.toString();
-        } else {
-            return "";
-        }
+        return new String(is.readAllBytes(), StandardCharsets.UTF_8);
     }
 }
