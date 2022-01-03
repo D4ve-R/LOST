5329cfef3278c9b006684593647fcc6d1dd9f6ee
Sebastian <sese.menne@web.de>
2022-01-03 18:07:44 +0100

    Changed the Swing LookAndFeel to better show the focused button and add enter to the usual action Keys.

--- macpan/app/src/main/java/lost/macpan/Main.java
+++ macpan/app/src/main/java/lost/macpan/Main.java
@@ -7,12 +7,23 @@
 package lost.macpan;
 
 
+import javax.swing.UIManager;
+
 /**
  * Main entry point for app
  */
 public class Main {
    public static void main(String [] args){
         // to invoke Swing thread safe
+
+       try {
+           // Set System L&F
+           UIManager.setLookAndFeel(
+                   UIManager.getSystemLookAndFeelClassName());
+       }
+       catch (Exception e) {
+            e.printStackTrace();
+       }
        java.awt.EventQueue.invokeLater(new Runnable(){
            public void run() {
                App app = new App();
